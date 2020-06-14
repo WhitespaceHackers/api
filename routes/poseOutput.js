61cb0f74242dc9fcfc093c81a1454cc240542127
jameshtlee <jameshtlee@live.com.sg>
2020-06-14 15:10:41 +0800

    add default json if no data from rpi

--- routes/poseOutput.js
+++ routes/poseOutput.js
@@ -2,10 +2,14 @@ var express = require('express');
 var router = express.Router();
 const storage = require('node-persist');
 
+// DEFAULT pose data
+var def = require('../default.json');
 /* GET pose data */
 router.get('/', async function(req, res) {
   let pose = await storage.getItem('pose');
-  res.json(pose);
+  pose ? res.json(pose) : res.json(def);
 });
+
+
 
 module.exports = router;
